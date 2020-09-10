f9e4748b53ec3d16f73d07ea19a45dc280a8a7a8
Pavel <BomGard@users.noreply.github.com>
2020-09-10 11:40:12 +0300

    загрузка адресов из файла, в .exe падает

--- geo.py
+++ geo.py
@@ -15,3 +15,15 @@ def getcoordinates(address):
         return pos[1] + " " + pos[0]
     else:
         return None
+
+
+def getcoordinates_from_excel(array):
+    arr_pos = []
+    for address in array:
+        result_url = 'https://geocode-maps.yandex.ru/1.x/?apikey=d02c5ac9-442e-43a3-8f34-4b641f189db6&format=json&geocode=' + address
+        response = requests.get(result_url)
+        todos = json.loads(response.text)
+        pos = todos['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')
+        result = pos[1] + " " + pos[0]
+        arr_pos.append(result)
+    return arr_pos
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+import pandas as pd
+import numpy as np
+from openpyxl.utils.dataframe import dataframe_to_rows
 import geo
 
 root = Tk()
@@ -14,10 +18,27 @@ def get_coordinates():
         messagebox.showerror('Борода', 'Введите адресс')
 
 
+def get_coordinates_form_exel():
+    # Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    try:
+        text_result.insert(0.0, 'Вычисляем координаты...' + '\n')
+        filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+        data = pd.read_excel(filename)
+        array = data['Address']
+        mat = geo.getcoordinates_from_excel(array)
+        data['Pos'] = mat
+        data.to_excel(filename, index=False)
+    except Exception:
+        messagebox.showerror('Борода', 'Что-то пошло не так')
+    else:
+        messagebox.showinfo('Result', "Координаты записали в тот же файл, можно смотерть")
+    pass
+
+
 # настройка окна приложения
 root['bg'] = 'grey'
 root.title("Геокодер")
-root.geometry('300x250')
+root.geometry('300x350')
 root.wm_attributes('-alpha', 0.9)
 # root.iconbitmap('icon.ico')
 root.resizable(width=False, height=False)
@@ -27,6 +48,7 @@ text = Label(text='Введите адрес', font='20', bg='#3d3d42', fg='#ccc
 data = Entry(root, font='Consolas 15', relief='solid', justify='center')
 # check_status = Checkbutton(text='Test_check')
 get_button = Button(text='Получить координаты', command=get_coordinates)
+get_button2 = Button(text='Загрузить файл c адресами', command=get_coordinates_form_exel)
 
 global text_result
 text_result = Text(root, width='20', height='10', wrap=WORD)
@@ -37,6 +59,7 @@ data.pack(pady=5)
 # check_status.pack()
 get_button.pack(pady=5)
 text_result.pack(pady=5)
+get_button2.pack(pady=5)
 
 # запуск приложения в цикле
 root.mainloop()
